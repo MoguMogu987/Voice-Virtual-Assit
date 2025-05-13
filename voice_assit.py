faacb15ed2cdc8532c427b2de41bf800fe897da4
FreePuffDaddy <ths.richard9@gmail.com>
2025-05-13 17:45:06 +0200

    voice assit simple

---
+++ voice_assit.py
@@ -0,0 +1,71 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AGENT_ID = os.getenv("AGENT_ID")
+API_KEY = os.getenv("API_KEY")
+
+from elevenlabs.client import ElevenLabs
+from elevenlabs.conversational_ai.conversation import Conversation
+from elevenlabs.conversational_ai.default_audio_interface import DefaultAudioInterface
+from elevenlabs.types import ConversationConfig
+
+"""
+Config de l'agent
+"""
+
+user_name = "Akhy"
+prompt = "parle comme bassem"
+first_message = f"oui allo salem {user_name} ?"
+
+conversation_override = {
+    "agent": {
+        "prompt": {
+            "prompt": prompt,
+        },
+        "first_message": first_message,
+    },
+}
+
+config = ConversationConfig(
+    conversation_config_override=conversation_override,
+    extra_body={},
+    dynamic_variables={},
+)
+
+client = ElevenLabs(api_key=API_KEY)
+
+conversation = Conversation(
+    client,
+    AGENT_ID,
+    config=config,
+    requires_auth=True,
+    audio_interface=DefaultAudioInterface(),
+)
+"""
+def des callback
+"""
+def print_agent_response(response):
+    print(f"Agent: {response}")
+
+def print_interrupted_response(original, corrected):
+    print(f"Agent interrupted, truncated response: {corrected}")
+
+def print_user_transcript(transcript):
+    print(f"User: {transcript}")
+"""
+lancement de la session
+"""
+conversation = Conversation(
+    client,
+    AGENT_ID,
+    config=config,
+    requires_auth=True,
+    audio_interface=DefaultAudioInterface(),
+    callback_agent_response=print_agent_response,
+    callback_agent_response_correction=print_interrupted_response,
+    callback_user_transcript=print_user_transcript,
+)
+
+conversation.start_session()
